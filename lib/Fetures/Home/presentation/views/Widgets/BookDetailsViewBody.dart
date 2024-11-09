8d4b780e7e3b2c34f098dbe404fbe9064430de3e
HadyAbdallah <ahady916@gmail.com>
2024-11-09 17:46:39 +0200

    Create similar books list view and add custom scroll view and separate code to sections

--- lib/Fetures/Home/presentation/views/Widgets/BookDetailsViewBody.dart
+++ lib/Fetures/Home/presentation/views/Widgets/BookDetailsViewBody.dart
@@ -1,8 +1,7 @@
-import 'package:bookly/Fetures/Home/presentation/views/Widgets/BookRating.dart';
-import 'package:bookly/Fetures/Home/presentation/views/Widgets/BooksAction.dart';
+import 'package:bookly/Fetures/Home/presentation/views/Widgets/BookDetailsSection.dart';
 import 'package:bookly/Fetures/Home/presentation/views/Widgets/CustomBookDetailsAppBar.dart';
-import 'package:bookly/Fetures/Home/presentation/views/Widgets/CustomBookItem.dart';
-import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/Fetures/Home/presentation/views/Widgets/SimilarBooksSection.dart';
+
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -10,46 +9,30 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.2),
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 42,
-          ),
-          const Text(
-            "The Jungle Book",
-            style: Styles.textStyle30,
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: 0.7,
-            child: Text(
-              "Rudyard Kipling",
-              style: Styles.textStyle16.copyWith(fontStyle: FontStyle.italic),
+    return const CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                CustomBookDetailsAppBar(),
+                BookDetailsSection(),
+                Expanded(
+                  child: SizedBox(
+                    height: 50,
+                  ),
+                ),
+                SimilarBooksSection(),
+                SizedBox(
+                  height: 40,
+                ),
+              ],
             ),
           ),
-          const SizedBox(
-            height: 14,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const BooksAction(),
-        ],
-      ),
+        )
+      ],
     );
   }
 }
