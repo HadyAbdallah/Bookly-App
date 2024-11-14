b42a78afe6401248747a423d5957870f89a4bd47
HadyAbdallah <ahady916@gmail.com>
2024-11-14 15:35:39 +0200

    Use Cached Network Image

--- lib/Fetures/Home/presentation/views/Widgets/CustomBookItem.dart
+++ lib/Fetures/Home/presentation/views/Widgets/CustomBookItem.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/core/Widgets/CustomLoadingIndicator.dart';
 import 'package:bookly/core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -6,15 +8,16 @@ class CustomBookImage extends StatelessWidget {
   final String imageURL;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            image: DecorationImage(
-              image: NetworkImage(imageURL),
-              fit: BoxFit.fill,
-            )),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          imageUrl: imageURL,
+          fit: BoxFit.fill,
+          placeholder: (context, url) => const CustomLoadingIndicator(),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
       ),
     );
   }
